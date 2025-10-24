97d081b155c4794a0e0336bad15a9eb3e16d6a76
drunazir <drunazir@gmail.com>
2025-10-24 14:00:16 +0100

    Update OMOP_Git_Training.R

--- OMOP_Git_Training.R
+++ OMOP_Git_Training.R
@@ -1,4 +1,15 @@
 install.packages("pak")
+install.packages("omopgenerics")
 pak::pkg_install(c("omopgenerics", "CodelistGenerator", "CDMConnector", "ohdsi/omock"))
 usethis::edit_r_environ()
+
+Sys.getenv("OMOP_DATA_FOLDER")
+library(omock)
+library(duckdb)
+cdm <- mockCdmFromDataset(datasetName = "empty_cdm", source = "duckdb")
+cdm
+library(CodelistGenerator)
+library(tibble)
+library(here)
+library(omopgenerics)
 
