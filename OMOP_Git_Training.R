cc1c4986956a9e598d220a11b5160b67479132ba
drunazir <drunazir@gmail.com>
2025-10-24 14:09:36 +0100

    Update OMOP_Git_Training.R

--- OMOP_Git_Training.R
+++ OMOP_Git_Training.R
@@ -1,3 +1,4 @@
+install.packages(c("dplyr","dbplyr","duckdb"))
 install.packages("pak")
 install.packages("omopgenerics")
 pak::pkg_install(c("omopgenerics", "CodelistGenerator", "CDMConnector", "ohdsi/omock"))
@@ -12,4 +13,13 @@ library(CodelistGenerator)
 library(tibble)
 library(here)
 library(omopgenerics)
+
+codes <- list(codelist_name = c(252942L, 257581L, 313236L)) |>
+  newCodelist()
+codes
+codes <- list(codelist_1 = c(252942L, 257581L, 313236L), codelist_2 = c(46269775L, 46269784L)) |>
+  newCodelist()
+codes
+codes <- getDrugIngredientCodes(cdm = cdm, name = "acetaminophen", routeCategory = "oral", nameStyle = "{concept_name}")
+codes
 
